958d1b9bfc59968b7effc88a9ccdb4c564b03dec
sixteenis <psm0103907@naver.com>
2024-10-08 17:00:47 +0900

    Initial Commit

---
+++ MangomMovie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MangomMovie
+//
+//  Created by 박성민 on 10/8/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
