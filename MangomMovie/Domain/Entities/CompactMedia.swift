adf7f0a0c184cc18ac31091cb0f19c6a9d206b27
NERO <mxzxro@gmail.com>
2024-10-11 20:10:29 +0900

    Entities 구조 변경

---
+++ MangomMovie/Domain/Entities/CompactMedia.swift
@@ -0,0 +1,17 @@
+//
+//  CompactMedia.swift
+//  MangomMovie
+//
+//  Created by NERO on 10/11/24.
+//
+
+import Foundation
+
+struct CompactMedia {
+    let type: MediaType
+    let id: Int
+    let imagePath: String
+    let genre: [String]
+    let title: String
+    let isFavorite: Bool
+}
---
+++ MangomMovie/Domain/Entities/MediaType.swift
@@ -0,0 +1,13 @@
+//
+//  MediaType.swift
+//  MangomMovie
+//
+//  Created by NERO on 10/11/24.
+//
+
+import Foundation
+
+enum MediaType {
+    case movie
+    case tv
+}
