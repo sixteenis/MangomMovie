dd88084b9869dbfb9786a49edc1b472c557bd6e9
NERO <mxzxro@gmail.com>
2024-10-13 01:59:22 +0900

    UseCase 구현

--- MangomMovie/Domain/UseCases/HomeUseCase.swift
+++ MangomMovie/Domain/UseCases/HomeUseCase.swift
@@ -14,34 +14,24 @@ protocol HomeUseCase {
     func fetchTrendTVList() -> Single<Result<[CompactMedia], Error>>
 }
 
-//final class DefaultHomeUseCase: HomeUseCase {
-//    private let trendRepository: TrendRepository
-//    private let favoriteRepository: FavoriteRepository
-//    
-//    init(trendRepository: TrendRepository, favoriteRepository: FavoriteRepository) {
-//        self.trendRepository = trendRepository
-//        self.favoriteRepository = favoriteRepository
-//    }
-//    
-<<<<<<< HEAD
-<<<<<<< HEAD
-//    func addFavoriteItem(_ item: CompactMedia) -> Bool {
-=======
-//    func addFavoriteItem(_ item: CompactMedia) {
->>>>>>> dac9e4c (UseCase Protocol 정의)
-=======
-//    func addFavoriteItem(_ item: CompactMedia) -> Bool {
->>>>>>> 723ea08 (Add Favorite 시 Bool값 반환하도록 수정 / fetchDetailItem 매개변수 수정)
-//        
-//    }
-//    
-//    func fetchTrendMovieList() -> Single<Result<[CompactMedia], Error>> {
-//        
-//        return
-//    }
-//    
-//    func fetchTrendTVList() -> Single<Result<[CompactMedia], Error>> {
-//        
-//        return
-//    }
-//}
+final class DefaultHomeUseCase: HomeUseCase {
+    private let trendRepository: TrendRepository
+    private let favoriteRepository: FavoriteRepository
+    
+    init(trendRepository: TrendRepository, favoriteRepository: FavoriteRepository) {
+        self.trendRepository = trendRepository
+        self.favoriteRepository = favoriteRepository
+    }
+
+    func addFavoriteItem(_ item: CompactMedia) -> Bool {
+        return favoriteRepository.addFavoriteItem(item)
+    }
+    
+    func fetchTrendMovieList() -> Single<Result<[CompactMedia], Error>> {
+        return trendRepository.fetchTrendMovieList()
+    }
+    
+    func fetchTrendTVList() -> Single<Result<[CompactMedia], Error>> {
+        return trendRepository.fetchTrendTVList()
+    }
+}
