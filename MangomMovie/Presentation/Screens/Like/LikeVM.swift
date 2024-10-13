89931aca0009be10349a4646fd3063d5c3d0232a
sixteenis <psm0103907@naver.com>
2024-10-13 22:50:23 +0900

    SearchVM 틀 구현

---
+++ MangomMovie/Presentation/Screens/Like/LikeVM.swift
@@ -0,0 +1,22 @@
+//
+//  LikeVM.swift
+//  MangomMovie
+//
+//  Created by 박성민 on 10/13/24.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+final class LikeVM: BaseViewModel {
+    struct Input {
+        
+    }
+    struct Output {
+        
+    }
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+}
