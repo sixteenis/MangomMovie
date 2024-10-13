ed90be8f3dbd4cf64cc2c8cf9a486513d4e331cb
Rob-Yoo <jin12243@naver.com>
2024-10-13 19:47:46 +0900

    WIP: TMDB용 DataSource 생성 완료, but 각각의 Repository 구현체는 아직...

---
+++ MangomMovie/Data/Repositories/Remote/DefaultTrendRepository.swift
@@ -0,0 +1,27 @@
+//
+//  DefaultTrendRepository.swift
+//  MangomMovie
+//
+//  Created by Jinyoung Yoo on 10/13/24.
+//
+
+import RxSwift
+
+//final class DefaultTrendRepository: TrendRepository {
+//    
+//    private let dataSource: MediaDataSource
+//    private let disposeBag = DisposeBag()
+//    
+//    init(dataSource: MediaDataSource = DefaultMediaDataSource()) {
+//        self.dataSource = dataSource
+//    }
+//    
+//    func fetchTrendMovieList() -> Single<Result<[CompactMedia], Error>> {
+//    
+//    }
+//    
+//    func fetchTrendTVList() -> Single<Result<[CompactMedia], Error>> {
+//        
+//    }
+//
+//}
